e9ad2742e0c54e3191055ac4b9ac0b4adea52fe2
Tom Banish <tombanish@gmail.com>
2019-11-26 01:29:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,11 +2,5 @@ def my_each(array)
   if block_given?
     i = 0
     
-    while i < array.length
-      yield array[i]
-      i += 1
-    end
-    
-    array
-  end
-end
+    while i > array.length
+      
